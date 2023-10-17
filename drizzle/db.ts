9cc371b42295585470ceb279a20939bcb2b2bdd8
Mark Teekens <mark@rubyfinance.nl>
2023-10-17 20:37:51 +0200

    feat: removed dotenv

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -1,11 +1,9 @@
 import { neon, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
-import * as dotenv from "dotenv";
-dotenv.config();
 
 neonConfig.fetchConnectionCache = true;
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon("***REMOVED***");
 const db = drizzle(sql);
 
 export default db;
