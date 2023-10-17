1ee4b76effffe060595c98d3cf9ce287b8a04b26
Mark Teekens <mark@rubyfinance.nl>
2023-10-17 20:35:08 +0200

    fix: add string

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 neonConfig.fetchConnectionCache = true;
 
-const sql = neon(process.env.DATABASE_URL!);
+const sql = neon("***REMOVED***");
 const db = drizzle(sql);
 
 export default db;
