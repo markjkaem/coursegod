4506b387fc0a7706fa43f7e364e565c671d52dea
Mark Teekens <mark@rubyfinance.nl>
2023-10-17 15:31:10 +0200

    rem unused code

--- src/routes/payment/index.tsx
+++ src/routes/payment/index.tsx
@@ -1,4 +1,4 @@
-import { Session } from "@auth/core/types";
+// import { Session } from "@auth/core/types";
 import { component$ } from "@builder.io/qwik";
 import { RequestHandler } from "@builder.io/qwik-city";
 import Stripe from "stripe";
@@ -7,15 +7,15 @@ const stripe = new Stripe(process.env.STRIPE_TEST_KEY!, {
   apiVersion: "2023-10-16",
 });
 export const onRequest: RequestHandler = async ({
-  sharedMap,
+  //   sharedMap,
   headers,
   query,
 }) => {
   headers.set("Cache-Control", "private");
-  const session: Session | null = sharedMap.get("session");
+  //   const session: Session | null = sharedMap.get("session");
   const lookupKey = query.get("id");
   console.log(lookupKey);
-  const email = session?.user?.email;
+  //   const email = session?.user?.email;
 
   const price = await stripe.prices.retrieve(`${lookupKey}`);
   console.log(price);
