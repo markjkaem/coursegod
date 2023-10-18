a93b06006de83d253393097783d75b2833e773dd
Mark Teekens <mark@rubyfinance.nl>
2023-10-19 00:36:17 +0200

    feat; add sign in

--- src/routes/sign-in/index.tsx
+++ src/routes/sign-in/index.tsx
@@ -1,7 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import { useAuthSignin } from "../plugin@auth";
 import { Form } from "@builder.io/qwik-city";
-import { Image } from "@unpic/qwik";
 
 export default component$(() => {
   const signIn = useAuthSignin();
