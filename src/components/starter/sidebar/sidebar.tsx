7c2b967e89ae9be513ab4de338f7535f94f0aa3a
Mark Teekens <mark@rubyfinance.nl>
2023-10-18 23:36:51 +0200

    st

--- src/components/starter/sidebar/sidebar.tsx
+++ src/components/starter/sidebar/sidebar.tsx
@@ -137,7 +137,7 @@ export default component$(({ hasAccesToCourses }: any) => {
                 >
                   <svg
                     aria-hidden="true"
-                    class="h-6 w-6 flex-shrink-0 text-gray-600 transition duration-75 "
+                    class="h-6 w-6 flex-shrink-0 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
                     fill="currentColor"
                     viewBox="0 0 20 20"
                     xmlns="http://www.w3.org/2000/svg"
@@ -170,7 +170,7 @@ export default component$(({ hasAccesToCourses }: any) => {
                   <div class="group flex items-center rounded-lg p-2 text-base font-normal text-gray-900   ">
                     <svg
                       aria-hidden="true"
-                      class="h-6 w-6 flex-shrink-0 text-gray-400 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 "
+                      class="h-6 w-6 flex-shrink-0 text-gray-600 transition duration-75 "
                       fill="currentColor"
                       viewBox="0 0 20 20"
                       xmlns="http://www.w3.org/2000/svg"
