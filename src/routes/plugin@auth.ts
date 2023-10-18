cf2c76b09c818303e2c512554355c0520263d7dc
Mark Teekens <mark@rubyfinance.nl>
2023-10-19 00:34:14 +0200

    feat: add tokens to data

--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -1,5 +1,7 @@
 import { serverAuth$ } from "@builder.io/qwik-auth";
 import Google from "@auth/core/providers/google";
+import Discord from "@auth/core/providers/discord";
+import TwitterProivider from "@auth/core/providers/twitter";
 import type { Provider } from "@auth/core/providers";
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import db  from "../../drizzle/db"
@@ -13,6 +15,15 @@ export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } =
       Google({
         clientId: env.get("GOOGLE_ID")!,
         clientSecret: env.get("GOOGLE_SECRET")!,
+      }),
+      Discord({
+        clientId: env.get("DISCORD_ID")!,
+        clientSecret: env.get("DISCORD_SECRET")!,
+      }),
+      TwitterProivider({
+        clientId: env.get("TWITTER_ID")!,
+        clientSecret: env.get("TWITTER_SECRET")!,
+  
       }),
     ] as Provider[],
   }));
