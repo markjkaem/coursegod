82a04a32fa04b7f858a3ad00686341fe3dcf2e27
Mark Teekens <mark@rubyfinance.nl>
2023-10-19 14:46:53 +0200

    feat: add env get

--- src/routes/payment/success/index.tsx
+++ src/routes/payment/success/index.tsx
@@ -16,6 +16,7 @@ export const onRequest: RequestHandler = async ({
   query,
   sharedMap,
   url,
+  env,
 }) => {
   const authSession: Session | null = sharedMap.get("session");
   if (!authSession || new Date(authSession.expires) < new Date()) {
@@ -31,7 +32,7 @@ export const onRequest: RequestHandler = async ({
 
   const portalSession = await stripe.billingPortal.sessions.create({
     customer: checkoutSession.customer as string,
-    return_url: `https://moneymaker.vercel.app/dashboard/subscriptions`,
+    return_url: `${env.get("BASE_URL")!}dashboard/subscriptions`,
   });
   throw redirect(301, portalSession.url);
 };
--- src/routes/payment/unsuccesfull/index.tsx
+++ src/routes/payment/unsuccesfull/index.tsx
@@ -3,7 +3,7 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
   return (
     <>
-      <div>
+      <div class="flex w-screen justify-center">
         <span>Order unsuccesfull</span>
       </div>
     </>
