1228102fab7c63ebd49a77c21bce39987ce349ca
Mark Teekens <mark@rubyfinance.nl>
2023-10-19 16:59:38 +0200

    feat: robots, sitemap, unsuccesfull page fix

--- src/routes/payment/unsuccesfull/index.tsx
+++ src/routes/payment/unsuccesfull/index.tsx
@@ -1,15 +1,14 @@
 import { component$ } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
+import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
-  const loc = useLocation();
-  setTimeout(() => {
-    loc.url.pathname.replace(`${loc.params}`, "/dashboard/subscriptions");
-  }, 2000);
   return (
     <>
       <div class="flex w-screen justify-center">
-        <span>Order was cancelled, you will be redirected</span>
+        <span>
+          Order was cancelled,{" "}
+          <Link href="/dashboard/subscriptions">go back</Link>
+        </span>
       </div>
     </>
   );
