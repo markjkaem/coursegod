fac0d46e69d7b28041910b46274cb8d3a2cfb5a2
Mark Teekens <mark@rubyfinance.nl>
2023-10-19 15:41:22 +0200

    feat: add faq questions

--- src/routes/payment/unsuccesfull/index.tsx
+++ src/routes/payment/unsuccesfull/index.tsx
@@ -1,10 +1,15 @@
 import { component$ } from "@builder.io/qwik";
+import { useLocation } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const loc = useLocation();
+  setTimeout(() => {
+    loc.url.pathname.replace(`${loc.params}`, "/dashboard/subscriptions");
+  }, 2000);
   return (
     <>
       <div class="flex w-screen justify-center">
-        <span>Order unsuccesfull</span>
+        <span>Order was cancelled, you will be redirected</span>
       </div>
     </>
   );
