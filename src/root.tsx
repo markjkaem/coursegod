b60ed82649cc75557d239b84d89fe1748280a337
Mark Teekens <mark@rubyfinance.nl>
2023-10-19 17:10:44 +0200

    feat: add hubspot bot

--- src/root.tsx
+++ src/root.tsx
@@ -25,6 +25,7 @@ export default component$(() => {
         <ServiceWorkerRegister />
       </head>
       <body lang="en">
+        {/*  <!-- Start of HubSpot Embed Code --> */}
         <script
           type="text/javascript"
           id="hs-script-loader"
