74b99f0c05fe03286d4ea8cbddf2c831bd66dd2b
Mark Teekens <mark@rubyfinance.nl>
2023-10-19 14:22:28 +0200

    feat: add rem unused

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,5 +1,5 @@
 import { component$, Slot, useStyles$ } from "@builder.io/qwik";
-import { RequestHandler, routeLoader$ } from "@builder.io/qwik-city";
+import { routeLoader$ } from "@builder.io/qwik-city";
 import Header from "~/components/starter/header/header";
 import Footer from "~/components/starter/footer/footer";
 import styles from "./styles.css?inline";
