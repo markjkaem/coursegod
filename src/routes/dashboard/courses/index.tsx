c51a6906d1395840ad81a9fb15b18259ed5b375c
Mark Teekens <mark@rubyfinance.nl>
2023-10-24 22:03:48 +0200

    feat: add videos

--- src/routes/dashboard/courses/index.tsx
+++ src/routes/dashboard/courses/index.tsx
@@ -3,6 +3,7 @@ import { component$ } from "@builder.io/qwik";
 import { Link, RequestHandler } from "@builder.io/qwik-city";
 import { courses } from "~/ultils/courses";
 import { currentSubscription } from "~/ultils/subscriptions";
+import { Image } from "@unpic/qwik";
 
 export const onRequest: RequestHandler = async ({
   sharedMap,
@@ -22,26 +23,61 @@ export const onRequest: RequestHandler = async ({
 };
 
 export default component$(() => {
+  const courses = [
+    {
+      id: 1,
+      title: "Fiverr Freelancing Course",
+      description: "Learn to freelance on Fiverr",
+      banner: "image1.png",
+    },
+    {
+      id: 2,
+      title: "SEO Course",
+      description: "Learn web Search Engine Optimalization",
+      banner: "image1.png",
+    },
+    {
+      id: 3,
+      title: "Shopify Drop Shipping Course",
+      description: "Learn Shopify dropshipping",
+      banner: "image1.png",
+    },
+    {
+      id: 4,
+      title: "Youtube Earning Course",
+      description: "Learn to master Youtube",
+      banner: "image1.png",
+    },
+  ];
   return (
     <>
-      <div class="grid grid-cols-1 justify-center gap-4 p-6 lg:grid-cols-2 xl:grid-cols-3 ">
-        {courses.map((course) => {
+      <div class="mr-4 grid grid-cols-1 justify-center gap-4 p-4 lg:grid-cols-2 xl:grid-cols-3 ">
+        {courses.map((course, i) => {
           return (
-            <div
-              key={course.id}
-              class="w-80 rounded-lg border border-gray-200 bg-slate-900  p-6 shadow"
-            >
-              <span class="mb-2 text-2xl font-bold tracking-tight text-white ">
-                {course.title}
-              </span>
+            <Link key={i} href={`/dashboard/courses/${course.title}`}>
+              <div
+                key={course.id}
+                class="  w-80 rounded-lg border border-gray-600 bg-slate-900 p-2  shadow hover:bg-slate-800"
+              >
+                <Image
+                  layout="constrained"
+                  width={200}
+                  height={200}
+                  class="w-80 rounded-t-md"
+                  src="/image1.png"
+                  alt="MW"
+                />
+                <h2 class="mb-2 text-xl font-bold tracking-tight text-white ">
+                  {course.title}
+                </h2>
 
-              <p class="mb-3 font-normal text-white">{course.title}</p>
-              <Link href={`/dashboard/courses/${course.title}`}>
+                <p class="mb-3 font-normal text-white">{course.description}</p>
+
                 <button class="flex items-center px-4 py-2 text-black">
-                  Download
+                  Start
                 </button>
-              </Link>
-            </div>
+              </div>
+            </Link>
           );
         })}
       </div>
--- src/ultils/courses.ts
+++ src/ultils/courses.ts
@@ -1,40 +1,92 @@
-export const courses = [{
+export const courses = [
+  {
     id: 1,
-    title: 'course1'
-}, {
+    title: "Fiverr Freelancing Course",
+    link: "https://mega.nz/folder/pykVmRDS#88Mcs-ZngFJy3Hx_IP3Vow",
+    description: "Learn to freelance on Fiverr",
+    banner: "image1.png",
+    chapters: [
+      {
+        id: 1,
+        title: "1. Account On Fiverr And Dashboard ",
+        source: "k51UnjoUJfd6SjzAoaM",
+      },
+      {
+        id: 2,
+        title: "2. Issues Face In Fiverr ",
+        source: "k1ycqpg2q6UspTzAoaN",
+      },
+      {
+        id: 3,
+        title: "3. Social Media Marketing Gig ",
+        source: "k4zho0R2e4teXlzAoaL",
+      },
+      {
+        id: 4,
+        title: "4. Facebook Marketing ",
+        source: "k7kREdBLnNpBA0zAoaK",
+      },
+      {
+        id: 5,
+        title: "5. How to define packages ",
+        source: "k20J8jupZvayvazAoaH",
+      },
+      {
+        id: 6,
+        title: "6. Scope and Pricing ",
+        source: "k56uk7vVLgx28LzAoaI",
+      },
+      {
+        id: 8,
+        title: "8. Important points of Gig ",
+        source: "k5fpN1fM8cYFaezAoaG",
+      },
+      {
+        id: 9,
+        title: "9. Gig gallery ",
+        source: "ktuWAvr2p5SuomzAoaF",
+      },
+      {
+          id: 10,
+          title: "10. Impressions ",
+          source: "k4SpMZT1vWxYwEzAoaE",
+        },
+    
+      {
+        id: 11,
+        title: "11. Define the offer scope ",
+        source: "k2P0cpYN8hrQIszAoma",
+      },
+    ],
+  },
+  {
     id: 2,
-    title: 'course2'
-},
-{
+    title: "SEO Course",
+    link: "https://mega.nz/folder/c3E0nAhK#gc9_am0__tVI4YO9Zju0ew",
+    description: "Learn web Search Engine Optimalization",
+    banner: "image1.png",
+    chapters: [
+     
+    ],
+  },
+  {
     id: 3,
-    title: 'course3'
-},
-{
+    title: "Shopify Drop Shipping Course",
+    link: "https://mega.nz/folder/Z3cxVQqA#33IaGHk7-PciURWky_kJtg",
+    description: "Learn Shopify dropshipping",
+    banner: "image1.png",
+    chapters: [
+     
+    ],
+  },
+  {
     id: 4,
-    title: 'course3'
-},
-{
-    id: 5,
-    title: 'course3'
-},
-{
-    id: 6,
-    title: 'course3'
-},
-{
-    id: 7,
-    title: 'course3'
-},
-{
-    id: 8,
-    title: 'course3'
-},
-{
-    id: 9,
-    title: 'course3'
-},
-{
-    id: 10,
-    title: 'course3'
-}
-]
+    title: "Youtube Earning Course",
+    link: "https://mega.nz/folder/svtEXa4b#lq53mc_hYQi0VrpYeDvDFw",
+    description: "Learn to master Youtube",
+    banner: "image1.png",
+    chapters: [
+      
+    ],
+  },
+];
