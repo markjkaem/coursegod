b56a41fd91810e1149240982edec78c06354afe2
Mark Teekens <mark@rubyfinance.nl>
2023-10-29 14:50:43 +0100

    feat: add courses, add env protection, deleted old key

--- src/routes/dashboard/courses/index.tsx
+++ src/routes/dashboard/courses/index.tsx
@@ -8,13 +8,14 @@ export const onRequest: RequestHandler = async ({
   sharedMap,
   redirect,
   url,
+  env,
 }) => {
   const authSession: Session | null = sharedMap.get("session");
   if (!authSession || new Date(authSession.expires) < new Date()) {
     throw redirect(302, `/sign-in?callbackUrl=${url.pathname}`);
   }
   const email = authSession.user?.email;
-  const response = await currentSubscription(email as string);
+  const response = await currentSubscription(email as string, env);
   const hasPageAcces = response.courses;
   if (!hasPageAcces) {
     throw redirect(303, "/dashboard/subscriptions");
@@ -27,13 +28,13 @@ export default component$(() => {
       id: 1,
       title: "Fiverr Freelancing Course",
       description: "Learn to freelance on Fiverr",
-      banner: "image1.png",
+      banner: "bannerfiver.png",
     },
     {
       id: 2,
-      title: "SEO Course",
-      description: "Learn web Search Engine Optimalization",
-      banner: "image1.png",
+      title: "Illustration for designers",
+      description: "Learn Illustration for designers",
+      banner: "bannerillustrations.png",
     },
     {
       id: 3,
@@ -56,14 +57,17 @@ export default component$(() => {
             <Link key={i} href={`/dashboard/courses/${course.title}`}>
               <div
                 key={course.id}
-                class="  w-80 rounded-lg border border-gray-600 bg-slate-900 p-2  shadow hover:bg-slate-800"
+                class="group relative w-80 rounded-lg border border-gray-600 bg-slate-900 p-2  shadow hover:bg-slate-800"
               >
+                <div
+                  class={`absolute left-0 top-0 z-20 h-[1px] bg-transparent group-hover:w-[2rem] group-hover:animate-moveline group-hover:bg-white`}
+                ></div>
                 <Image
                   layout="constrained"
                   width={200}
                   height={200}
-                  class="w-80 rounded-t-md"
-                  src="/image1.png"
+                  class="h-52 w-80 rounded-t-md object-cover"
+                  src={`/${course.banner}`}
                   alt="MW"
                 />
                 <h2 class="mb-2 text-xl font-bold tracking-tight text-white ">
