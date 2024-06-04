37401d8120c31f182e013e0559a3eb153ef58c9b
markjkaem <markteek@gmail.com>
2024-06-04 16:03:48 +0200

    dsds

--- drizzle/db.ts
+++ drizzle/db.ts
@@ -3,7 +3,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 
 neonConfig.fetchConnectionCache = true;
 
-const sql = neon("***REMOVED***");
+const sql = neon("postgres://tsxsoftware:ZHbX4OaV7MRi@ep-ancient-mountain-63172778.eu-central-1.aws.neon.tech/moneymaker?sslmode=require");
 const db = drizzle(sql);
 
 export default db;
